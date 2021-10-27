1670265f07f8319dd342b5af987d6feef8afd538
Frederic Lenaerts <frederic.lenaerts@nexuzhealth.com>
2021-10-27 09:29:50 +0200

    bug

---
+++ src/stories/button/button.stories.js
@@ -0,0 +1,14 @@
+import Documentation from './button.docs.mdx';
+import { ButtonComponent } from './button.component';
+
+export default {
+  title: 'Components/Button',
+  component: ButtonComponent,
+  parameters: {
+    docs: {
+      page: Documentation,
+    },
+  },
+};
+
+export const Default = {};
